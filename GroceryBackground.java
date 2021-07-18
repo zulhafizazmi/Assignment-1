a8823b2d7ce4611e868fdcd14ebee5100b950203
zulhafizazmi <81027436+zulhafizazmi@users.noreply.github.com>
2021-07-18 10:47:15 +0800

    Update GroceryBackground.java

--- GroceryBackground.java
+++ GroceryBackground.java
@@ -2,13 +2,14 @@ package Assignment1;
 
 import java.util.Scanner;
 
-public class GroceryBackground {
+public class GroceryBackground { //superclass
 	
 	protected String name;
 	protected String location;
 	protected String contact;
 	
-	GroceryBackground() {
+	GroceryBackground() { //cunstructor no argument
+		
 		this.name = "Watsun";
 		this.location = "Tumpat";
 		this.contact = "097193642";
@@ -24,4 +25,5 @@ public class GroceryBackground {
 	}
 
 }
+
 
